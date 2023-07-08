cedd1b65964273c25484a00abf78c618e4c2dc5c
johnmiicheal <michealelijah301@gmail.com>
2023-07-08 07:50:14 +0100

    removed incorrect motion params

--- pages/app.tsx
+++ pages/app.tsx
@@ -4,6 +4,8 @@ import Layout from "../src/components/App/Layout";
 import { IoPaperPlane, IoPaperPlaneOutline } from "react-icons/io5";
 import { useRouter } from "next/router";
 import { motion } from 'framer-motion';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 const App = () => {
     const router = useRouter();
@@ -17,9 +19,6 @@ const App = () => {
       mt={28}
       ml={{ base: "1000px", md: "600px", lg: "200px" }}
       align="center"
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5 }}
     >
       <motion.div
         initial={{ opacity: 0 }}
@@ -158,145 +157,11 @@ const App = () => {
         borderRadius="md"
         cursor="pointer"
         onClick={() => router.push('/app/chat')}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
       >
         Start Chat
         <Icon as={IoPaperPlaneOutline} w="5" h="5" />
       </Flex>
     </Flex>
-
-
-        {/* <Flex direction="column" w="1200px" mt={28} ml={{ base: "1000px", md: "600px", lg: "200px" }} align="center">
-          <Text
-            fontSize="50"
-            bgGradient="linear(to-r, #00F0FF, #53AF28)"
-            bgClip="text"
-            fontWeight={600}
-          >
-            Lecture Mate
-          </Text>
-          <Image src="/guides.png" w={{base: "800px", md: "800px", lg: "1000px"}} pointerEvents="none" />
-          <Flex gap={4} mt={5} textAlign="center">
-            <Flex direction="column" w="300px" gap={5}>
-              <Text fontSize={20} fontWeight={600}>
-                Usage
-              </Text>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-                color="#333333"
-              >
-                "Generate 3 theory questions on the topic [insert topic here]"{" "}
-              </Box>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-                color="#333333"
-              >
-                "List 10 items of [insert question] and explain them"
-              </Box>
-            </Flex>
-            <Flex direction="column" w="300px" gap={5}>
-              <Text fontSize={20} fontWeight={600}>
-                Tips
-              </Text>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-                color="#333333"
-              >
-                Always remember to copy the token before asking any question
-              </Box>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-              >
-                <Text>
-                  Merged PDF is the best way to use Lecture Mate, you can merge
-                  your pdfs{" "}
-                  <Link
-                    href="https://www.ilovepdf.com/merge_pdf"
-                    target="_blank"
-                    color="#005103"
-                    fontWeight={600}
-                  >
-                    here
-                  </Link>
-                </Text>
-              </Box>
-            </Flex>
-            <Flex direction="column" w="300px" gap={5}>
-              <Text fontSize={20} fontWeight={600}>
-                Limitations
-              </Text>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-                color="#333333"
-              >
-                Calculations are not supported yet, but we'll keep working on
-                that
-              </Box>
-              <Box
-                bg="#E2F0E2"
-                display="flex"
-                alignItems="center"
-                height="100px"
-                px={2}
-                py={2}
-                borderRadius="md"
-                color="#333333"
-              >
-                File uploads are limited to one pdf per time, uploading a new
-                file clears the chat history{" "}
-              </Box>
-            </Flex>
-          </Flex>
-
-          <Flex
-            w="800px"
-            mt={28}
-            boxShadow="md"
-            justify="space-between"
-            bg="#E2F0E2"
-            color="#808680"
-            _hover={{ border: "1px solid #005103 "}}
-            p={5}
-            borderRadius="md"
-            cursor="pointer"
-            onClick={() => router.push('/app/chat')}
-          >
-            Start Chat
-            <Icon as={IoPaperPlaneOutline} w="5" h="5" />
-          </Flex>
-        </Flex> */}
       </Flex>
     </Flex>
   );
