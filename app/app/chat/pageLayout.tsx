c5ee92454bfdddbafd6f5f022477e81d8bcb40d7
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-12-08 20:26:20 +0100

    Positioned the remove pdf button

--- app/app/chat/pageLayout.tsx
+++ app/app/chat/pageLayout.tsx
@@ -499,6 +499,7 @@ const Chat = ({user2}: any) => {
               {pdfList.map((pdf, index)=> (
                 <Flex
                 key={index}
+                position={"relative"}
                 h="50px"
                 mt={5}
                 gap={2}
@@ -520,7 +521,7 @@ const Chat = ({user2}: any) => {
                 <Text noOfLines={1} w='70%' textOverflow="ellipsis" key={index}>
                   {pdf.book_name}
                 </Text>
-                <Icon as={IoRemoveCircleOutline} onClick={() => handleRemovePdf(pdf.id, pdf.book_name, index)} ml={'10%'} mr={'3%'} _hover={pdf.book_name === selectedPdf ? {color: "white", bg: "#3C7C1C",  borderRadius: '100%'} : {color: "white", bg: "#53AF28",  borderRadius: '100%'}} w="5" h="5" />
+                <Icon as={IoRemoveCircleOutline} onClick={() => handleRemovePdf(pdf.id, pdf.book_name, index)} position={"absolute"} top={"50%"} right={3} transform={"translateY(-50%)"} _hover={pdf.book_name === selectedPdf ? {color: "white", bg: "#3C7C1C",  borderRadius: '100%'} : {color: "white", bg: "#53AF28",  borderRadius: '100%'}} w="5" h="5" />
               </Flex>
               ))
                 
