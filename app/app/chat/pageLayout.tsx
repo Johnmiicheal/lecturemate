4ce459ae48ec88075ef08e228f0299dfa374bc39
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-12-12 22:08:58 +0100

    Edited regex

--- app/app/chat/pageLayout.tsx
+++ app/app/chat/pageLayout.tsx
@@ -129,7 +129,9 @@ const Chat = ({user2}: any) => {
   }
 
   const extractQuestion = (content: any) => {
-    const regexPattern1 = /Question:\/\/--(.*?)(?=(--\/\/))/;
+    const regexPattern1 = 
+    /\s*Question\s*:\/\/--(.*?)--\/\//;
+    // /Question:\/\/--(.*?)(?=(--\/\/))/;
     const matchResult1 = content.match(regexPattern1);
     const textBeforePattern1 = matchResult1 ? matchResult1[1].trim() : content;
 
