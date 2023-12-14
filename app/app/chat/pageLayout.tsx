c679250fa1420158772c6f74f1ec5f68ccebd6f3
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-12-14 04:51:34 +0100

    Removed dormant page

--- app/app/chat/pageLayout.tsx
+++ app/app/chat/pageLayout.tsx
@@ -617,7 +617,7 @@ const Chat = ({user2}: any) => {
                 <Text noOfLines={1} w='70%' textOverflow="ellipsis" key={index}>
                   {pdf.book_name}
                 </Text>
-                <Icon as={IoRemoveCircleOutline} onClick={() => handleRemovePdf(pdf.id, pdf.book_name, index)} position={"absolute"} zIndex={"20"} top={"50%"} right={3} transform={"translateY(-50%)"} _hover={pdf.book_name === selectedPdf ? {color: "white", bg: "#3C7C1C",  borderRadius: '100%'} : {color: "white", bg: "#53AF28",  borderRadius: '100%'}} w="5" h="5" />
+                <Icon as={IoRemoveCircleOutline} onClick={(e) =>{e.stopPropagation(); handleRemovePdf(pdf.id, pdf.book_name, index)}} position={"absolute"} zIndex={"20"} top={"50%"} right={3} transform={"translateY(-50%)"} _hover={pdf.book_name === selectedPdf ? {color: "white", bg: "#3C7C1C",  borderRadius: '100%'} : {color: "white", bg: "#53AF28",  borderRadius: '100%'}} w="5" h="5" />
               </Flex>
               ))
                 
